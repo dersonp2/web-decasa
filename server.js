c74e73a7f32fb4c9b084761318d850ea1ddf0506
Anderson <anderson@lsdi.ufma.br>
2020-04-27 08:33:58 -0300

    configuração heroku

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+//Importa as dependências que acabamos de instalar
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/web-decasa'));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/web-decasa'));
 
-app.get('/*', function(req, res) {
-  res.sendFile('/index.html', {root: 'dist/web-decasa/'}
-);
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/web-decasa/index.html'));
 });
 
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
