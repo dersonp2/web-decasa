2e0cef27df76fb52467b9075b6dc65d38d0c1d7e
Anderson <anderson@lsdi.ufma.br>
2020-06-16 22:15:10 -0300

    Configuração para heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   API_URL: 'http://homologacao.sistemadecasa.com.br/v1/rest/decasa/',
+  API_MAPS: 'https://maps.googleapis.com/maps/api/geocode/json',
   ACESS_TOKEN: 'G416F208V208U416V1196D780E416U1196Y884W416H1144H1196H364H676X780K936G416G936V832O416G416C416V1144H1196H',
-  API_MAPS: 'AIzaSyAoVvj5lM77pVqV4EyRzJwIHoBK83MGumU'
+  CEP_URL: 'https://viacep.com.br/ws',
+  KEY_MAPS: 'AIzaSyAofSgPCKs-NUy2_AVLw-opQiPm75wR030'
 };
