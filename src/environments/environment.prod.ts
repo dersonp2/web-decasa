b0f2e1c87ebfac572794bb728b8601310ba62804
Anderson <anderson@lsdi.ufma.br>
2020-06-16 22:42:50 -0300

    adicionando o https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://homologacao.sistemadecasa.com.br/v1/rest/decasa/',
+  API_URL: 'https://homologacao.sistemadecasa.com.br/v1/rest/decasa/',
   API_MAPS: 'https://maps.googleapis.com/maps/api/geocode/json',
   ACESS_TOKEN: 'G416F208V208U416V1196D780E416U1196Y884W416H1144H1196H364H676X780K936G416G936V832O416G416C416V1144H1196H',
   CEP_URL: 'https://viacep.com.br/ws',
