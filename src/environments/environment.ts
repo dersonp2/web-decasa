71086f7c1d3db9baef9856e307cc7bcef20d4bf2
Anderson <anderson@lsdi.ufma.br>
2020-06-29 07:58:08 -0300

    Salvando endereco

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   // API_URL: 'http://homologacao.sistemadecasa.com.br/v1/rest/decasa',
   // http://localhost:8080/v1/rest/decasa/
   // '/v1/rest/decasa'
-  API_URL: '/v1/rest/decasa',
+   API_URL: '/v1/rest/decasa',
+  // API_URL: 'http://localhost:8080/v1/rest/decasa',
   API_MAPS: 'https://maps.googleapis.com/maps/api/geocode/json',
   ACESS_TOKEN: 'G416F208V208U416V1196D780E416U1196Y884W416H1144H1196H364H676X780K936G416G936V832O416G416C416V1144H1196H',
   CEP_URL: 'https://viacep.com.br/ws',
